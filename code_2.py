d4e23f1bccde92d4cf94c5500f68e24a57fffd58
zasuklotur <143523477+zasuklotur@users.noreply.github.com>
2023-10-27 22:11:54 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,43 @@
+if __name__ == "__main__":
+def generate_random_data():
+import random
+    return data
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+def main():
+
+
+    main()
+def generate_random_data():
+
+    main()
+    return data
+import random
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    for item in data:
+if __name__ == "__main__":
+}
+
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+
+    const data = generateRandomData();
+    });
+    }
+        console.log("Random Number: " + item);
+    return data;
+function main() {
+}
+    data.forEach(item => {
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
