cb5cb5909b318111058ffd31b7834d65ab4781c4
zasuklotur <143523477+zasuklotur@users.noreply.github.com>
2023-10-31 00:36:58 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,27 +1,13 @@
-    for item in data:
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-def generate_random_data():
+            System.out.println("Random Number: " + item);
+        }
+        }
 
-if __name__ == "__main__":
-import random
+        int[] data = new int[10];
+public class RandomDataGenerator {
+}
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
 
-    data = generate_random_data()
-def main():
-    return data
-
-        print(f"Random Number: {item}")
-
-
-
-if __name__ == "__main__":
-
-def main():
-    data = generate_random_data()
-    main()
-    return data
-    for item in data:
-import random
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
+        for (int item : data) {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
